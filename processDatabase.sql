eacd742f6d9ea447bccf07f7ef5d573ce6ce2ed2
Nguyen Tuan Khoa <75377239+khoa2212@users.noreply.github.com>
2022-01-13 20:02:06 +0700

    Add files via upload

--- processDatabase.sql
+++ processDatabase.sql
@@ -8,10 +8,21 @@ create table ManageTime
 	ID int,
     StartTime time,
     EndTime time,
+    StartTimeAgain time,
 
     constraint PK_ManageTime primary key (ID)
 );
 
+create table ManageKeyBoard
+(
+	inputDate date,
+    inputTime time,
+    content nvarchar(255)
+);
 insert ManageTime
-values(1, '6:00', '16:00');
-select ID, Time_format(StartTime, '%H:%i') , Time_format(EndTime, '%H:%i') from ManageTime
+values(1, '6:00', '23:59', '20:00');
+select ID, Time_format(StartTime, '%H:%i') , Time_format(EndTime, '%H:%i'), Time_format(StartTimeAgain, '%H:%i') from ManageTime;
+
+update ManageTime set EndTime = '19:55' where ID = 1;
+select * from ManageKeyBoard
+
